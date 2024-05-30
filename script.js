1f694b8e580efee63e90aba603c3c714fa6a4755
Mariana Aguilar <mara.aglr@gmail.com>
2024-05-30 11:26:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ let numeroEntrada = document.getElementById("numeroEntrada");
 
 let mensaje = document.getElementById("mensaje");
 
+let intento = document.getElementById("intentos")
+let intentos = 0;
+
 function resultado() {
 
     let numeroIngresado = parseInt(numeroEntrada.value);
+    intentos++
+    intento.textContent = intentos;
+    intento.style.color = "blue";
 
     if (numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)) {
         mensaje.textContent= "El dato ingresado debe ser un número de 1 al 100";
+        mensaje.style.color = "black";
         return
     };
     
