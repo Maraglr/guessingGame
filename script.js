2db10b7fab388b155db0a0ec738a6da64902106f
Mariana Aguilar <mara.aglr@gmail.com>
2024-05-30 11:07:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let random = Math.floor(Math.random() * 100) + 1;
+
+let numeroEntrada = document.getElementById("numeroEntrada");
+
+let mensaje = document.getElementById("mensaje");
+
+function resultado() {
+
+    let numeroIngresado = parseInt(numeroEntrada.value);
+
+    if (numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)) {
+        mensaje.textContent= "El dato ingresado debe ser un número de 1 al 100";
+        return
+    };
+    
+    if(numeroIngresado === random) {
+        mensaje.textContent= "Felicidades has adivinado el número";
+        mensaje.style.color = "green";
+        numeroEntrada.disable = true;
+    } else if (numeroIngresado < random) {
+        mensaje.textContent= "El número es mayor al que dijiste";
+        mensaje.style.color = "red";
+    } else {
+        mensaje.textContent= "El número es menor al que dijiste";
+        mensaje.style.color = "red"; 
+    };
+
+};    
